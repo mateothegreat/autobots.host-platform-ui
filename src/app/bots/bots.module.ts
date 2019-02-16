0e720a58eddb5e2a96c6af2cf0bb5348c48dcdfc
Matthew Davis <matthew@matthewdavis.io>
2019-02-15 19:20:57 -0500

    bot search

--- src/app/bots/bots.module.ts
+++ src/app/bots/bots.module.ts
@@ -1,15 +1,15 @@
-import { CommonModule }              from '@angular/common';
-import { NgModule }                  from '@angular/core';
-import { RouterModule }              from '@angular/router';
-import { SharedModule }              from '../shared/shared.module';
-import { BotsDeployCreateComponent } from './bots-deploy/bots-deploy-create/bots-deploy-create.component';
-import { BotsDeployWidgetComponent } from './bots-deploy/bots-deploy-widget/bots-deploy-widget.component';
-import { BotsDeployComponent }       from './bots-deploy/bots-deploy.component';
-import { BotsSearchComponent }       from './bots-search/bots-search.component';
-import { BotsComponent }             from './bots.component';
-import { BotsDeployCreateInfoComponent } from './bots-deploy/bots-deploy-create/bots-deploy-create-info/bots-deploy-create-info.component';
+import { CommonModule }                        from '@angular/common';
+import { NgModule }                            from '@angular/core';
+import { RouterModule }                        from '@angular/router';
+import { SharedModule }                        from '../shared/shared.module';
+import { BotsDeployCreateInfoComponent }       from './bots-deploy/bots-deploy-create/bots-deploy-create-info/bots-deploy-create-info.component';
 import { BotsDeployCreateRepositoryComponent } from './bots-deploy/bots-deploy-create/bots-deploy-create-repository/bots-deploy-create-repository.component';
-import { BotsDeployCreateSettingsComponent } from './bots-deploy/bots-deploy-create/bots-deploy-create-settings/bots-deploy-create-settings.component';
+import { BotsDeployCreateSettingsComponent }   from './bots-deploy/bots-deploy-create/bots-deploy-create-settings/bots-deploy-create-settings.component';
+import { BotsDeployCreateComponent }           from './bots-deploy/bots-deploy-create/bots-deploy-create.component';
+import { BotsDeployWidgetComponent }           from './bots-deploy/bots-deploy-widget/bots-deploy-widget.component';
+import { BotsDeployComponent }                 from './bots-deploy/bots-deploy.component';
+import { BotsSearchComponent }                 from './bots-search/bots-search.component';
+import { BotsComponent }                       from './bots.component';
 
 @NgModule({
 
@@ -54,6 +54,11 @@ import { BotsDeployCreateSettingsComponent } from './bots-deploy/bots-deploy-cre
                     }, {
 
                         path: 'deploy/:typeId',
+                        component: BotsDeployCreateComponent
+
+                    }, {
+
+                        path: 'manage/:uuid',
                         component: BotsDeployCreateComponent
 
                     }, {
